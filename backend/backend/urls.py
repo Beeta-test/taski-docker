7510797933434c38ebe96631efa5903ecd443a02
Maxim Cherdansev <HeikouSeen@yandex.ru>
2024-08-24 17:27:51 +0300

    setup

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
+
 from api import views
 
 router = routers.DefaultRouter()
@@ -10,3 +11,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
 ]
+
